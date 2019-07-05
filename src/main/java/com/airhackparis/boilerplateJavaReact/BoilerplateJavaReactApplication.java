dc405221994b07f82effa1de93d94b1634fbaf1c
Gilbert Tchoi <g17569832+gilberttchoi@users.noreply.github.com>
2019-07-05 16:05:26 +0200

    first commit

---
+++ src/main/java/com/airhackparis/boilerplateJavaReact/BoilerplateJavaReactApplication.java
@@ -0,0 +1,13 @@
+package com.airhackparis.boilerplateJavaReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoilerplateJavaReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoilerplateJavaReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airhackparis/boilerplateJavaReact/BoilerplateJavaReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airhackparis.boilerplateJavaReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoilerplateJavaReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
